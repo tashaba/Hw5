d04990f19f41d798ad31d9f0e48f7862d6b8026e
tashaba <35852644+tashaba@users.noreply.github.com>
2018-02-25 23:17:44 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -1,40 +1,29 @@
-could only create an array...
-
-var circle1= [100,200,100]
-var circle2= [300,200,30]
-
-
-
-
+var x = [];
+var y = [];
+var d = [];
+var c = []
 function setup() {
-  createCanvas(400, 400);
-  colorMode(HSB)
+createCanvas (400,400);
+background(255, 255, 153);
+for (var i = 0; i < 100; i++) {
+x[i] = random(20, 380);
+y[i] = random(30, 380);
+d[i] = random(20, 50);
+c[i] = random(102, 255, 102);
 }
-  
+}
+
 function draw() {
-  background(255);
-  noStroke();
-
-	
-	for (var i = 0; i < 50; i = i + 1) {
-  fill(120, 60, 100);
-  ellipse(circle1[0], circle1[1], circle1[2]);
-  
-  fill(240, 60, 100);
-  ellipse(circle2[0], circle2[1], circle2[2]);
-	}
-  // 1% of the time
-  if (random() < 0.01) {
-    // random diameter between 10 and 400
-    circle1[2] = random(10, 400);
-  }
-  
-  // 2% of the time…
-  if (random() < 0.02) {
-    // random diameter between 10 and 400
-    circle2[2] = random(10, 400);
-  }
+noStroke();
+// ellipses
+for (var i = 0; i < 50; i++) {
+fill(c[i]);
+ellipse(x[i], y[i], d[i]);
+if (random() < 0.01) {
+// random diameter between 10 and 400
+d[i] = random(10, 200);
+}}
+if (random() < 50) {
+d[i] = random(20, 90);
 }
-
-  
- 
+}
